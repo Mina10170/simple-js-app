126028056b4f0621f1e790ca2764ee7892653902
Mina10170 <89662271+Mina10170@users.noreply.github.com>
2021-09-06 14:10:41 +0200

    Update scripts.js
    If applied, this commit will create a new variable called pokemonList and assign to it a array consists of  three objects. This array contains Pokémon data to display in my application,  Each object represents one Pokémon and needs to have the same keys to avoid errors, keys used for each pokemon object name, height and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Burger';
-document.write(favoriteFood);
+// list of Pokemons
+let pokemonList=[
+{name:"balbasure", height:"7", type:['grass', 'poison']},
+{name: "Ivysaur", height:"1", type:['grass', 'poison']},
+{name: "Venusaur", height"2", type:['grass', 'poison']}]
