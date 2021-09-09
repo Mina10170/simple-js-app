43ececb51d2219d56b738a630ac2139afd7119f7
Mina10170 <89662271+Mina10170@users.noreply.github.com>
2021-09-09 23:10:37 +0200

    Update scripts.js
    IF applied, this commit will amend the height value data type

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 // list of Pokemons
 let pokemonList=[
-{name:"balbasure", height:"7", type:['grass', 'poison']},
-{name: "Ivysaur", height:"1", type:['grass', 'poison']},
-{name: "Venusaur", height"2", type:['grass', 'poison']}]
+{name:"balbasure",height:7, type:['grass', 'poison']},
+{name: "Ivysaur", height:1, type:['grass', 'poison']},
+{name: "Venusaur",height:2, type:['grass', 'poison']}
+]
 for (let i = 0; i < pokemonList.length; i++) {
   // Created variables containing each pokemon height and name.
   let pokemonName = pokemonList[i].name;
   let pokemonHeight = pokemonList[i].height;
-
   if (pokemonHeight >= 7) {
     document.write(
       '<p>' +
@@ -16,7 +16,7 @@ for (let i = 0; i < pokemonList.length; i++) {
         '(Height: ' +
         pokemonHeight +
         ')' +
-        " - Wow that's big!" +
+        "Wow that's big!" +
         '</p>'
     );
   } else {
