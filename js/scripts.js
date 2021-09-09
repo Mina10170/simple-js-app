802b306a4e06313d1bee123ecb859cc72493f85c
Mina10170 <89662271+Mina10170@users.noreply.github.com>
2021-09-09 22:23:14 +0200

    scripts.js Update
    If applied, this commit  Creates a for loop to iterate through the pokemon list.
    Added a conditional within the loop to display the pokemon with a height
    // greater than 7

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,25 @@ let pokemonList=[
 {name:"balbasure", height:"7", type:['grass', 'poison']},
 {name: "Ivysaur", height:"1", type:['grass', 'poison']},
 {name: "Venusaur", height"2", type:['grass', 'poison']}]
+for (let i = 0; i < pokemonList.length; i++) {
+  // Created variables containing each pokemon height and name.
+  let pokemonName = pokemonList[i].name;
+  let pokemonHeight = pokemonList[i].height;
+
+  if (pokemonHeight >= 7) {
+    document.write(
+      '<p>' +
+        pokemonName +
+        ' ' +
+        '(Height: ' +
+        pokemonHeight +
+        ')' +
+        " - Wow that's big!" +
+        '</p>'
+    );
+  } else {
+    document.write(
+      '<p>' + pokemonName + ' ' + '(Height: ' + pokemonHeight + ')' + '</p>'
+    );
+  }
+}
