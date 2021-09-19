121b302daa15768e16e0778b8312132da8cbd709
Mina10170 <89662271+Mina10170@users.noreply.github.com>
2021-09-19 21:42:11 +0200

    Update scripts.js
    If applied, this commit in the “scripts.js” file of my project, wrap my pokemonList array in an IIFE to avoid accidentally accessing the global state.
    Before anything else, I created a new pokemonRepository variable to hold what my IIFE will return, then assign the IIFE to that variable.
    The IIFE should return an object with the following public functions assigned as keys:
    getAll: return all items (pokemonRepository.getAll(); should return the pokemonList array)
    add: add a single item to the pokemonList array (calling pokemonRepository.add(item); should add the Pokémon referred to with item to the pokemonList array)

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,26 @@ for (let i = 0; i < pokemonList.length; i++) {
 pokemonList.forEach(function(pokemonList) {
   console.log(pokemonList.name + ' is ' + pokemonList.height + ' Feet height.');
 });
+let pokemonRepository = (function () {
+  let pokemonList = [
+{name:"balbasure",height:7, type:['grass', 'poison']},
+{name: "Ivysaur", height:1, type:['grass', 'poison']},
+{name: "Venusaur",height:2, type:['grass', 'poison']}
+];
+
+  function add(item) {
+   return pokemonList.push(item);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+pokemonRepository.getAll();
+console.log(pokemonRepository.getAll());
+
