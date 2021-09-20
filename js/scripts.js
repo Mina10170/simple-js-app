fe502e8fc60eeb60a6f36caa0246f453171b8617
Mina10170 <89662271+Mina10170@users.noreply.github.com>
2021-09-20 21:32:48 +0200

    Update scripts.js
    If applied, this commit created a pokemonRepository IIFE and it is supposed to be a replacement of my previous code except for the forEach loop that just created.

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,13 @@
 // list of Pokemons
-let pokemonList=[
-{name:"balbasure",height:7, type:['grass', 'poison']},
-{name: "Ivysaur", height:1, type:['grass', 'poison']},
-{name: "Venusaur",height:2, type:['grass', 'poison']}
-]
-for (let i = 0; i < pokemonList.length; i++) {
-  // Created variables containing each pokemon height and name.
-  let pokemonName = pokemonList[i].name;
-  let pokemonHeight = pokemonList[i].height;
-  if (pokemonHeight >= 7) {
-    document.write(
-      '<p>' +
-        pokemonName +
-        ' ' +
-        '(Height: ' +
-        pokemonHeight +
-        ')' +
-        "Wow that's big!" +
-        '</p>'
-    );
-  } else {
-    document.write(
-      '<p>' + pokemonName + ' ' + '(Height: ' + pokemonHeight + ')' + '</p>'
-    );
-  }
-}
 pokemonList.forEach(function(pokemonList) {
   console.log(pokemonList.name + ' is ' + pokemonList.height + ' Feet height.');
 });
 let pokemonRepository = (function () {
-  let pokemonList = [
+let pokemonList = [
 {name:"balbasure",height:7, type:['grass', 'poison']},
 {name: "Ivysaur", height:1, type:['grass', 'poison']},
 {name: "Venusaur",height:2, type:['grass', 'poison']}
 ];
-
   function add(item) {
    return pokemonList.push(item);
   }
@@ -48,6 +21,7 @@ let pokemonRepository = (function () {
     getAll: getAll
   };
 })();
-pokemonRepository.getAll();
-console.log(pokemonRepository.getAll());
+ pokemonRepository.getAll().forEach(pokemon)
+
+
 
